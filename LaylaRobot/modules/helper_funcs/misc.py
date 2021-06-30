25d198ce3b027838b08908ebf1658aba5efcb0e1
The Style King <78583783+Style-King@users.noreply.github.com>
2021-06-30 10:39:58 +0530

    Update misc.py

--- LaylaRobot/modules/helper_funcs/misc.py
+++ LaylaRobot/modules/helper_funcs/misc.py
@@ -65,16 +65,13 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
-        pairs = [
-        modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
-    ]
+        pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Back", callback_data="layla_back"),
+             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
-    round_num = len(modules) / 3
-    calc = len(modules) - round(round_num)
-    if calc == 1:
-        pairs.append((modules[-1],))
-    elif calc == 2:
-        pairs.append((modules[-1],))
+    else:
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
 
     return pairs
 
