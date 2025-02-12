b0f3a9d384be74decd81680398e5b0337bfd48a8
Mahan M V <73701328+mahanmvofficial@users.noreply.github.com>
2025-02-12 18:34:50 +0530

    Update sample_config.py

--- LaylaRobot/sample_config.py
+++ LaylaRobot/sample_config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 797768146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = 7598661  # integer value, dont use ""
+    API_HASH = "925a979e9ea55b7fe91fc30931813592"
+    TOKEN = "7415632841:AAHdJMWKOr_edaTktJYUaAYZywYRb0vVUC0"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1346612506  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "HEROGAMERS1" 
-    SUPPORT_CHAT = "AwesomeSupport"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "Kannada_Dominators"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001392103597
+        -1002450743778
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001392103597
+        -1002337195825
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
